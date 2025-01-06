e868e8705df71f6edeab49ac8826ff3839334c83
Mehul Uttam <mehulsavio@gmail.com>
2025-01-06 10:41:46 +0530

    Commit

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] arr = {3,2,5,4,1,4,9};
+        int[] arr = {1,2,3,5,4};
         bubble(arr);
         System.out.println(Arrays.toString(arr));
 
@@ -16,7 +16,7 @@ public class BubbleSort {
         for (int i = 0; i < arr.length; i++){
             swapped = false;
             //for each step, max item will come at the last respective index
-            for (int j = 1; j< arr.length - i - 1; j++){
+            for (int j = 1; j< arr.length - i ; j++){
                 //swap if the item is smaller than the previous item
                 if (arr[j] < arr[j - 1]){
                     int temp = arr[j];
