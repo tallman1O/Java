55f27bec963e5a92f0982637e571b951cbcbf320
Mehul Uttam <mehulsavio@gmail.com>
2025-01-08 01:23:01 +0530

    Commit

--- OOP/classes.java
+++ OOP/classes.java
@@ -4,6 +4,7 @@ public class classes {
     public static void main(String[] args) {
         Student Student1;
         Student1 = new Student();
+        Student1 = new Student(24,"Mehul", 87.5f);
         //or
         // Student Student1 = new Student();
         System.out.println(Student1.rnoll); // 0
@@ -13,9 +14,9 @@ public class classes {
         //After putting Default Value of Marks
         System.out.println(Student1.marks); // 90.0
 
-        Student1.rnoll = 69;
-        Student1.name = "Mehul";
-        Student1.marks = 96.5f;
+//        Student1.rnoll = 69;
+//        Student1.name = "Mehul";
+//        Student1.marks = 96.5f;
 
         System.out.println(Student1.rnoll); // 69
         System.out.println(Student1.name); // Mehul
@@ -25,7 +26,10 @@ public class classes {
         //constructor
         Student Student2 = new Student();
 
-        Student2.greeting(); //Hello My name is Mehul Uttam
+        Student2.greeting();
+        Student2.changeName("Mehul Savio");
+        Student2.greeting();
+
 
     }
 
@@ -42,9 +46,19 @@ class Student {
         System.out.println("Hello My name is "+ name);
     }
 
+    void changeName(String newName) {
+        name = newName;
+    }
+
     Student(){
     this.rnoll = 12;
     this.name = "Mehul Uttam";
     this.marks = 56.7f;
+    }
+
+    Student(int rnoll, String name, float marks){
+        this.rnoll = rnoll;
+        this.name = name;
+        this.marks = marks;
     }
 }
